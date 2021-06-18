c26822e191b9750fb8cc0f7fd0c2151ef097de88
rovull <yaskovetc@gmail.com>
2021-06-18 13:51:42 +0300

    qerty

--- src/Repository/AnnouncementRepository.php
+++ src/Repository/AnnouncementRepository.php
@@ -5,7 +5,7 @@ namespace App\Repository;
 
 use App\Entity\Announcement;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\Persistence\ManagerRegistry;
 use Doctrine\DBAL\DBALException;
 
 
